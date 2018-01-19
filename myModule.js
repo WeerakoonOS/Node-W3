18dedc89bd1826736e77c1544cdc4a6a7288212e
WeerakoonOS <osw0294@gmail.com>
2018-01-20 04:16:10 +0530

    t

---
+++ myModule.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('C:/Users/Oshani Weerakoon/Documents/Node-W3/firstModule');
+//absolute path, can require absolute within npm_module folder
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);
